88f0d26322f61a6578c853dd73490f99ff88a034
PgLee227 <pglee227@gmail.com>
2021-08-20 15:22:14 +0800

    3.0
    using ipynb  to store information.

--- import pandas.py
+++ import pandas.py
@@ -5,11 +5,11 @@ import scipy
 from scipy.integrate import simps
 
 ## import data into py
-array_0 = pd.read_csv('G:/aug/8-10/29/40-5.csv',skiprows=45,nrows=5000)#draw data from .csv
+array_0 = pd.read_csv('F:/Data/Linewidth——reducation/8-18/25/10-5.csv',skiprows=45,nrows=5000)#draw data from .csv
 array_0 = array_0.values # dataframe --> array
-array_1 = pd.read_csv('G:/aug/8-10/29/40-50.csv',skiprows=545,nrows=4500)
+array_1 = pd.read_csv('F:/Data/Linewidth——reducation/8-18/25/10-50.csv',skiprows=545,nrows=4500)
 array_1 = array_1.values # dataframe --> array
-X_trans=0.309; #the voltage/1V
+X_trans=0.865; #the voltage/1V
 #print(array_1.shape) print(array_0.shape)
 
 ## Define function
